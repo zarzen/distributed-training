9b5f153c00de0a31e7908663cc26482b1bfd9130
Zhen Zhang <zhangzhen.dev@gmail.com>
2019-11-01 01:11:32 -0400

    run single batch

--- test_scripts/pytorch_resnet50_cifar10.py
+++ test_scripts/pytorch_resnet50_cifar10.py
@@ -174,7 +174,8 @@ def train(epoch):
               disable=not verbose) as t:
         for batch_idx, (data, target) in enumerate(train_loader):
             adjust_learning_rate(epoch, batch_idx)
-
+            if batch_idx > 0:
+                return 
             if args.cuda:
                 data, target = data.cuda(), target.cuda()
             optimizer.zero_grad()
