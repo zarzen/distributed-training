207858e924e1e8a6873eb3503966a40aa5b7ca7a
Zhen Zhang <zhangzhen.dev@gmail.com>
2020-02-21 22:58:41 +0000

    disable sync_e

--- test_scripts/pytorch_resnet50_imagenet.py
+++ test_scripts/pytorch_resnet50_imagenet.py
@@ -185,11 +185,11 @@ def train(epoch):
             for i in range(0, len(data), args.batch_size):
                 data_batch = data[i:i + args.batch_size]
                 target_batch = target[i:i + args.batch_size]
-                sync_e()
+                # sync_e()
                 lobj = {"ph": "X", "name": "foward", "ts": time.time(), "pid": hvd.rank(), "dur": 0}
                 output = model(data_batch)
                 # event record 
-                sync_e()
+                # sync_e()
                 lobj["dur"]=time.time()-lobj["ts"]
                 model_logger.info(json.dumps(lobj))
 
@@ -208,10 +208,10 @@ def train(epoch):
                 lobj["dur"]=time.time()-lobj["ts"]
                 model_logger.info(json.dumps(lobj))
 
-                sync_e()
+                # sync_e()
                 lobj = {"ph": "X", "name": "backward", "ts": time.time(), "pid": hvd.rank(), "dur": 0}
                 loss.backward()
-                sync_e()
+                # sync_e()
                 lobj["dur"]=time.time()-lobj["ts"]
                 model_logger.info(json.dumps(lobj))
 
